14c2e070735fc216ebefb3b380e29761bd3e1468
Lucas <tucaskohlermfa.com>
2022-06-09 13:26:37 -0300

    teste de onblur desfeito

--- js/script.js
+++ js/script.js
@@ -1,12 +1,32 @@
 function revelarSenha() {
-    const img = document.getElementById('img')
-    const senha = document.getElementById('text3')
-    if (senha.type == 'password') {
-        senha.type = 'text'
+    const img = document.querySelector('img')
+    const input = document.querySelector('#text3')
+    if (input.type === 'password') {
+        input.type = 'text'
         img.setAttribute('src', 'images/eye-slashed.svg')
-
     } else {
-        senha.type = 'password'
+        input.type = 'password'
         img.setAttribute('src', 'images/eye.svg')
     }
 }
+
+// function mostrarBotao () {
+//     let img = document.querySelector('img')
+//     const css = window.getComputedStyle(img, null).display
+
+//     if (css === "none") {
+//         document.querySelector('img').className = "appear"
+//         return css;
+//     }
+    
+// }
+
+// function tirarBotao () {
+//     let img = document.querySelector('img')
+//     const css = window.getComputedStyle(img, null).display
+
+//     if (css === "block") {
+//         document.querySelector('img').className = "disappear";
+//         return css;
+//     }
+    
