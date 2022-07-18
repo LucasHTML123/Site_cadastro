38e90381676ce60a3b58f15190f834dc4cde8697
Lucas <tucaskohlermfa.com>
2022-07-18 20:33:01 -0300

    Ínicio JS para salvar dados(precariamente)

--- js/script.js
+++ js/script.js
@@ -12,13 +12,52 @@ function revelarSenha() {
 
 // Fazer ele salvar os dados no navegador!
 
-// function guardaDados() {
-//     const nome = document.querySelector('#password').value;
-//     const paragrafoErro = document.querySelector('#erro')
-//     function guardaNome() {
-//         if (!nome) {
-//             return paragrafoErro.innerHTML = 'FAVOR COLOQUE SEU NOME';
-//         }
-//     }
-// }
-// guardaDados()
+function guardaDados() {
+    const nome = document.querySelector('#name').value;
+    const email = document.querySelector('#email').value;
+    const senha = document.querySelector('#password').value;
+    const paragrafoErro = document.querySelector('#erro')
+    if (!nome) {
+        paragrafoErro.innerHTML = 'Favor preencher seu nome';
+        paragrafoErro.style.color = 'red';
+        paragrafoErro.style.backgroundColor = 'rgb(255, 152, 152)'
+        return;
+    } else {
+        paragrafoErro.innerHTML = ''
+    }
+
+    if (!email) {
+        paragrafoErro.innerHTML = 'Favor preencher seu e-mail';
+        paragrafoErro.style.color = 'red';
+        paragrafoErro.style.backgroundColor = 'rgb(255, 152, 152)'
+        return;
+    } else {
+        paragrafoErro.innerHTML = ''
+    }
+
+    if (!senha) {
+        paragrafoErro.innerHTML = 'Favor preencher sua senha';
+        paragrafoErro.style.color = 'red';
+        paragrafoErro.style.backgroundColor = 'rgb(255, 152, 152)'
+        return;
+    } else {
+        paragrafoErro.innerHTML = ''
+    }
+
+    if(localStorage.getItem('nome' || 'email' || 'senha')) {
+        paragrafoErro.innerHTML = 'Já estão cadastrados usuários neste computador!';
+        paragrafoErro.style.color = 'crimson';
+        paragrafoErro.style.backgroundColor = 'rgb(255, 152, 152)';    
+        return;
+    }
+
+    localStorage.setItem('nome', nome);
+    localStorage.setItem('email', email);
+    localStorage.setItem('senha', senha);
+}
+
+window.addEventListener('keypress', function(e) {
+    if(e.code === 'Enter') {
+        return guardaDados();
+    }
+})
