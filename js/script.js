36981341ee01b3ab4c1d576d3522e85c65f54b74
Lucas <tucaskohlermfa.com>
2022-07-18 19:08:52 -0300

    Possíveis correções

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 function revelarSenha() {
     const img = document.querySelector('img#eye')
-    const input = document.querySelector('#text3')
+    const input = document.querySelector('#password')
     if (input.type === 'password') {
         input.type = 'text'
         img.setAttribute('src', 'images/eye-slashed.svg')
@@ -8,4 +8,17 @@ function revelarSenha() {
         input.type = 'password'
         img.setAttribute('src', 'images/eye.svg')
     }
-}  
+}
+
+// Fazer ele salvar os dados no navegador!
+
+// function guardaDados() {
+//     const nome = document.querySelector('#password').value;
+//     const paragrafoErro = document.querySelector('#erro')
+//     function guardaNome() {
+//         if (!nome) {
+//             return paragrafoErro.innerHTML = 'FAVOR COLOQUE SEU NOME';
+//         }
+//     }
+// }
+// guardaDados()
