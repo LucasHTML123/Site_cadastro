1afac6e55ed999f35d7b3d21c011deb1a14d2245
tucaskohlermfa@gmail.com <tucaskohlermfa.com>
2022-07-19 13:25:02 -0300

    Checagem de dados no login.html

--- js/scriptLogin.js
+++ js/scriptLogin.js
@@ -1,5 +1,5 @@
 function revelarSenha() {
-    const senha = document.getElementById('text4')
+    const senha = document.getElementById('password')
     const img = document.getElementById('img')
     if (senha.type == 'password') {
         senha.type = 'text'
@@ -8,4 +8,27 @@ function revelarSenha() {
         senha.type = 'password'
         img.setAttribute('src', '../images/eye.svg')
     }
+}
+
+function checarDados() {
+    const nome = document.querySelector('#email').value;
+    const senha = document.querySelector('#password').value;
+    const senhaCadastrada = localStorage.getItem('senha');
+    const emailCadastrado = localStorage.getItem('email');
+    let paragrafoErro = document.querySelector('#erro')
+
+    if(!nome || !senha) {
+        paragrafoErro.innerHTML = 'Favor preencher seus dados corretamente';
+        return;
+    } else {
+        if(nome === emailCadastrado && senha === senhaCadastrada) {
+            paragrafoErro.innerHTML = '';
+            window.location.assign('https://google.com.br')
+            return;
+        } else {
+            paragrafoErro.innerHTML = 'Os dados digitados estão incorretos';
+            return;
+        }
+    }
+
 }
